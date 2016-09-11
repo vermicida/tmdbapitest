f06c0c483f6274f4f199fc3c5c71f635b0ec1289
Diego Herrera <vermicida@gmail.com>
2016-09-11 14:22:21 +0200

    Removed the The Movie Database API key.

--- scripts/constants/settings.js
+++ scripts/constants/settings.js
@@ -1,7 +1,7 @@
 /** 'Settings' constant class */
 var settingsConstant = {
     theMovieDatabaseApiVersion: "3",
-    theMovieDatabaseApiKey: "826b523c417cbb888744b13031d846c2"
+    theMovieDatabaseApiKey: "set-your-api-key-here"
 };
 
 /** Adds the 'Settings' constant definition to the Application Injector */
